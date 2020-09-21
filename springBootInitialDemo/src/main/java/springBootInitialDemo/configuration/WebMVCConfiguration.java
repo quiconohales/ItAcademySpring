f001ebb2e857aa7bcf5ac7b831d79d975bd3372d
quiconohales <quiconohales@gmail.com>
2020-09-21 11:24:24 +0200

    copia diaria

--- springBootInitialDemo/src/main/java/springBootInitialDemo/configuration/WebMVCConfiguration.java
+++ springBootInitialDemo/src/main/java/springBootInitialDemo/configuration/WebMVCConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 // Francesc Nohales
+@SuppressWarnings("deprecation")
 @ComponentScan(basePackages = {"springBootInitialDemo"})
 @Configuration
 @EnableWebMvc
