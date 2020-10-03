345af6f022d106e05f86ca02fee6c2f77868ec55
quiconohales <quiconohales@gmail.com>
2020-10-03 22:18:46 +0200

    Creación Proyecto Ejercicio 13 It Academy Botiga Cuadres

---
+++ BotigaCuadres/src/main/java/com/init/botiga/BotigaCuadresApplication.java
@@ -0,0 +1,13 @@
+package com.init.botiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotigaCuadresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotigaCuadresApplication.class, args);
+	}
+
+}
---
+++ BotigaCuadres/src/test/java/com/init/botiga/BotigaCuadresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.init.botiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotigaCuadresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
